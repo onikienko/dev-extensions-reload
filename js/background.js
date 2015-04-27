28b0f050b6c66473e460fb0260b994295d0dd56e
Michael Onikienko <michael.onikienko@gmail.com>
2015-04-27 11:20:28 +0300

    v1

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
             var default_options = storage.default_options,
                 option,
                 new_options = {};
-            
+
             for (option in default_options) {
                 if (!stored_options.hasOwnProperty(option)) {
                     new_options[option] = default_options[option];
@@ -41,25 +41,43 @@ chrome.runtime.onInstalled.addListener(function (details) {
 });
 
 chrome.runtime.onUpdateAvailable.addListener(function () {
-    // when an update is available - reload extension
-    // update will be install immediately
     chrome.runtime.reload();
 });
 
 
 function reload() {
-    function reEnable(extension) {
+    function reEnable(extension, option_page_tabs) {
         chrome.management.setEnabled(extension.id, false, function () {
-            chrome.management.setEnabled(extension.id, true);
+            chrome.management.setEnabled(extension.id, true, function () {
+                // check opened options page
+                option_page_tabs.forEach(function (tab) {
+                    if (tab.ext_id === extension.id) {
+                        chrome.tabs.create({
+                            windowId: tab.windowId,
+                            index: tab.index,
+                            url: tab.url,
+                            active: tab.active,
+                            pinned: tab.pinned
+                        });
+                    }
+                });
+            });
         });
     }
 
     chrome.management.getAll(function (extensions) {
-        extensions.forEach(function (extension) {
-            if (extension.enabled && extension.installType === "development" && extension.shortName !== "Dev extensions reload") {
-                reEnable(extension);
-            }
+        // check if there are tabs with options pages
+        chrome.tabs.query({url: 'chrome-extension://*/*'}, function (tabs) {
+            tabs.forEach(function (tab) {
+                tab.ext_id = tab.url.match(/^chrome-extension:\/\/([\s\S]*?)\//).pop();
+            });
+            extensions.forEach(function (extension) {
+                if (extension.enabled && extension.installType === "development" && extension.shortName !== "Dev extensions reload") {
+                    reEnable(extension, tabs);
+                }
+            });
         });
+
     });
     storage.area.get('reload_tabs', function (stored_option) {
         if (stored_option.reload_tabs) {
@@ -74,7 +92,7 @@ chrome.commands.onCommand.addListener(function (command) {
     }
 });
 
-function contMenu () {
+function contMenu() {
     storage.area.get('context_menu', function (stored_option) {
         if (stored_option && stored_option.context_menu) {
             chrome.contextMenus.create({
--- js/options.js
+++ js/options.js
@@ -1,8 +1,38 @@
 var main_tabs = new Tabs('#main_tabs');
 
-document.addEventListener('optionSaved', function (event) {
-    // reload extension after toggle session watcher option
-    if (event.detail.val.hasOwnProperty('context_menu') && event.detail.success) {
-        chrome.runtime.reload();
+document.addEventListener('optionsPageReady', function () {
+    var browser = navigator.userAgent.indexOf('OPR/') === -1 ? 'chrome' : 'opera',
+        chrome_link = 'chrome://extensions/configureCommands',
+        opera_link = 'opera://extensions/configureCommands',
+        opera_dev_link = 'opera://settings/configureCommands';
+
+    if (browser !== 'opera') {
+        document.querySelector('#opera_next').remove();
     }
-}, false);
+    document.querySelector('#linker').addEventListener('click', function (e) {
+        var target = e.target;
+
+        if (target.tagName === 'A') {
+            if (target.getAttribute('id') === 'ks_link') {
+                chrome.tabs.create({
+                    url: browser !== 'opera' ? chrome_link : opera_link,
+                    active: true
+                });
+                e.preventDefault();
+            }
+            if (target.getAttribute('id') === 'opera_next_link') {
+                chrome.tabs.create({
+                    url: opera_dev_link,
+                    active: true
+                });
+                e.preventDefault();
+            }
+        }
+    });
+    document.addEventListener('optionSaved', function (event) {
+        // reload extension after toggle session watcher option
+        if (event.detail.val.hasOwnProperty('context_menu') && event.detail.success) {
+            chrome.runtime.reload();
+        }
+    });
+});
